50df91d7350a1db20ffca74ffbb106834a5fc796
jjsanchezr <sistemas@flotaoccidental.com>
2016-02-27 13:27:32 -0500

    Apellido separado

--- HolaM_LeonardoCC/src/main/java/beans/AlumnoArq.java
+++ HolaM_LeonardoCC/src/main/java/beans/AlumnoArq.java
@@ -2,13 +2,22 @@ package beans;
 
 public class AlumnoArq {
 	private String nombre;
+	private String apellido;
 	
 	public String getNombre() {
 		return nombre;
 	}
 
+	public String getApellido() {
+		return apellido;
+	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}	
 
 }
